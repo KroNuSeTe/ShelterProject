50ef5d58e33cc0675fcc16d64296985c2ebc4fed
Jean Grey <jean6rey@gmail.com>
2025-11-11 19:58:10 +0100

    Update, lot of changes

---
+++ Source/ShelterProject/Public/AbilitySystem/Abilities/ShProjectileSpell.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ShGameplayAbility.h"
+#include "ShProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHELTERPROJECT_API UShProjectileSpell : public UShGameplayAbility
+{
+	GENERATED_BODY()
+};
---
+++ Source/ShelterProject/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHELTERPROJECT_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+};
